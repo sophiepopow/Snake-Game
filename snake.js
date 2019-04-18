66d20f0d8996509c79af64223918d9e4d993c51b
Sophie Popow <sophie.popow@gmail.com>
2019-04-18 23:52:45 +0200

    changing draw function

--- snake.js
+++ snake.js
@@ -14,8 +14,13 @@ let score = 0;
 
 function draw() {
   context.drawImage(background,0,0);
-  for(let i = 0; i < snake.length; i++)
+  for(let i = 0; i < snake.length; i++){
     context.fillStyle = ( i == 0 )? "green" : "white";
     context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+  context.drawImage(food, food.x, food.y);
+  context.fillStyle = "white";
+  context.font = "45px Sans Serif";
+  context.fillText(score, 2*box, 1.6*box);
 }
 let game = setInterval(draw, 100);
